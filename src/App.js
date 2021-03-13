636568391dd440b7c34bced561a872809534f1ee
ihor1998 <ihor.milshyn@gmail.com>
2021-03-13 21:26:35 +0200

    new-project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+import Header from './components/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title">Оберіть Ваш факультет та курс</h1>
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <div className="selection-container">
+      <select>
+        <option>Факультет</option>
+      </select>
+      <select>
+        <option>Курс</option>
+      </select>
+
+      <div>&times</div>
+    </div>
+  );
+}
+
+export default Header;
